f60127de3a1e4ee0dfae8537e1229799afac669f
christaddeucci <chris.taddeucci@gmail.com>
2021-10-28 22:16:28 -0400

    fixed player movement and added multiple rooms

--- Souls 'N Tombs/Assets/Player/PlayerMovement3D.cs
+++ Souls 'N Tombs/Assets/Player/PlayerMovement3D.cs
@@ -8,20 +8,76 @@ public class PlayerMovement3D : MonoBehaviour
 
     //Source: https://learn.unity.com/tutorial/controlling-unity-camerab-behaviour-2019-3#
 
-    public float speed = 20;
+    public float speed = 20f;
     private Vector3 motion;
-    private Rigidbody rb;
+    //private Rigidbody rb;
+
+    private CharacterController characterController;
+
+ 
+
+    [SerializeField] GameObject Player;
+
+
+    
+    
 
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        //rb = GetComponent<Rigidbody>();
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         motion = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        rb.velocity = motion * speed; 
+        //rb.velocity = motion * speed; 
+        //characterController.attachedRigidbody.velocity = motion * speed; //https://docs.unity3d.com/ScriptReference/CharacterController.html
+        //characterController.Move(motion*speed);
+        Vector3 movement = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up) * motion;
+        
+
+        //mouse location (aiming)
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        transform.Rotate(Vector3.up, mouseX * 10f);
+
+
+
+
+        //wasd movement
+        // if(motion != Vector3.zero && Input.GetKey("w") ){ //https://docs.unity3d.com/ScriptReference/CharacterController.Move.html
+        //     gameObject.transform.forward = motion;
+        // }
+
+        // if(motion != Vector3.zero){ //https://docs.unity3d.com/ScriptReference/CharacterController.Move.html
+        //     gameObject.transform.forward = motion;
+        // }
+        // if(Input.GetKey("w")){
+        //     transform.position += transform.forward;
+        // } else if(Input.GetKey("s")){
+        //     transform.position -= transform.forward;
+        // }else if(Input.GetKey("a")){
+        //     transform.position -= transform.right;
+        // }else if(Input.GetKey("d")){    
+        //     transform.position += transform.right;
+        // }    
+
+        if(motion != Vector3.zero && Input.GetKey("w" ) &! Input.GetKey("s")  &! Input.GetKey("a") &! Input.GetKey("d")){
+            gameObject.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement.normalized), 0.2f); //https://answers.unity.com/questions/803365/make-the-player-face-his-movement-direction.html
+            gameObject.transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        } else if (Input.GetKey("s") || Input.GetKey("a") || Input.GetKey("d")){
+             characterController.Move(motion*speed*Time.deltaTime);
+        }
+
+
+       
+
+        
+     
+
     }
 }
--- Souls 'N Tombs/Assets/Player/PlayerTracker.cs
+++ Souls 'N Tombs/Assets/Player/PlayerTracker.cs
@@ -8,32 +8,39 @@ public class PlayerTracker : MonoBehaviour
 
     //Source: https://learn.unity.com/tutorial/controlling-unity-camera-behaviour-2019-3#5e60f749edbc2a002071df05
     public Transform trackedObject;
-    public float maxDistance = 7;
-    public float moveSpeed = 5;
-    public float updateSpeed = 10;
-    [Range(0,10)]
-    public float currentDistance = 7;
+    [SerializeField] float maxDistance = 20;
+    [SerializeField] float moveSpeed = 5;
+    [SerializeField] float updateSpeed = 10;
+    [Range(0,20)]
+    [SerializeField] float currentDistance = 15;
     private string moveAxis = "Mouse ScrollWheel";
     private GameObject ahead;
-    private MeshRenderer _renderer;
+    //private MeshRenderer _renderer;
     public float hideDistance = 1.5f;
 
 
     void Start()
     {
         ahead = new GameObject("ahead");
-        _renderer = trackedObject.gameObject.GetComponent<MeshRenderer>();
+        //_renderer = trackedObject.gameObject.GetComponent<MeshRenderer>();
+        //transform.Rotate
 
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
+       //ahead.transform.position = trackedObject.position + trackedObject.forward * (maxDistance * 0.25f);
        ahead.transform.position = trackedObject.position + trackedObject.forward * (maxDistance * 0.25f);
        currentDistance += Input.GetAxisRaw(moveAxis) * moveSpeed * Time.deltaTime;
        currentDistance = Mathf.Clamp(currentDistance, 0, maxDistance);
+       //transform.position = Vector3.MoveTowards(transform.position, trackedObject.position + Vector3.up * currentDistance - trackedObject.forward * (currentDistance + maxDistance * 0.5f), updateSpeed * Time.deltaTime);
        transform.position = Vector3.MoveTowards(transform.position, trackedObject.position + Vector3.up * currentDistance - trackedObject.forward * (currentDistance + maxDistance * 0.5f), updateSpeed * Time.deltaTime);
+       //ahead.transform = transform.position.y;
+       //ahead.transform.position.
        transform.LookAt(ahead.transform);
-       _renderer.enabled = (currentDistance > hideDistance);
+      
+      
+       //_renderer.enabled = (currentDistance > hideDistance);
     }  
 }
